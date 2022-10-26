1aeff45abb8e2e11bbfd2508a6c23a5bca4dce07
Daniel Watkins <daniel@daniel-watkins.co.uk>
2022-10-26 09:43:04 -0400

    refactor non-web code into lib.rs

---
+++ src/lib.rs
@@ -0,0 +1,78 @@
+use std::cmp::Ordering;
+use std::collections::HashMap;
+use std::fs::File;
+use std::io::BufReader;
+
+use fapt::commands;
+use fapt::sources_list;
+use fapt::system::System;
+
+use tabled::{builder::Builder, Style};
+
+pub fn init_system(sources_list: String) -> Result<System, anyhow::Error> {
+    // Setup the system
+    let mut system = System::cache_only()?;
+    commands::add_builtin_keys(&mut system);
+    system.add_sources_entries(sources_list::read(BufReader::new(
+        File::open(&sources_list).unwrap(),
+    ))?);
+
+    system.set_arches(vec!["amd64"]);
+    system.update()?;
+    Ok(system)
+}
+
+pub fn do_madison(package: String, system: &System) -> Result<String, anyhow::Error> {
+    // Collect all the versions
+    let mut versions: HashMap<String, String> = HashMap::new();
+    for downloaded_list in system.listings()? {
+        for section in system.open_listing(&downloaded_list)? {
+            let pkg = section?.as_pkg()?;
+            if let Some(bin) = pkg.as_bin() {
+                let resolved_source = if let Some(bin_src) = &bin.source {
+                    bin_src
+                } else {
+                    &pkg.name
+                };
+                if resolved_source == &package {
+                    let key = downloaded_list.release.req.codename.to_owned();
+                    if let Some(current_value) = versions.get_mut(&key) {
+                        if deb_version::compare_versions(current_value, &pkg.version)
+                            == Ordering::Greater
+                        {
+                            *current_value = pkg.version.to_owned();
+                        }
+                    } else {
+                        versions.insert(key, pkg.version.to_owned());
+                    }
+                }
+            }
+        }
+    }
+    println!("{:?}", versions);
+
+    let mut output_builder = Builder::default();
+    let mut sorted_by_version: Vec<_> = versions.iter().collect();
+
+    sorted_by_version.sort_by(|(_, v1), (_, v2)| deb_version::compare_versions(v1, v2));
+
+    for (codename, codename_version) in sorted_by_version {
+        output_builder.add_record(vec![
+            package.to_owned(),
+            codename_version.to_owned(),
+            codename.to_owned(),
+            "source".to_string(),
+        ]);
+    }
+    Ok(format!(
+        "{}\n",
+        output_builder
+            .build()
+            .with(Style::empty().vertical('|'))
+            .to_string()
+            .lines()
+            .map(|line| line.trim())
+            .collect::<Vec<&str>>()
+            .join("\n")
+    ))
+}
--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,11 @@
 #[macro_use]
 extern crate rocket;
 
-use std::cmp::Ordering;
-use std::collections::HashMap;
-use std::fs::File;
-use std::io::BufReader;
-
 use rocket::serde::Deserialize;
 
-use fapt::commands;
-use fapt::sources_list;
 use fapt::system::System;
 
-use tabled::{builder::Builder, Style};
+use madison_rs::{do_madison, init_system};
 
 #[derive(Deserialize)]
 #[serde(crate = "rocket::serde")]
@@ -31,72 +24,7 @@ fn madison(
 ) -> Result<String, rocket::response::Debug<anyhow::Error>> {
     let system = &state.system;
     system.update()?;
-
-    // Collect all the versions
-    let mut versions: HashMap<String, String> = HashMap::new();
-    for downloaded_list in system.listings()? {
-        for section in system.open_listing(&downloaded_list)? {
-            let pkg = section?.as_pkg()?;
-            if let Some(bin) = pkg.as_bin() {
-                let resolved_source = if let Some(bin_src) = &bin.source {
-                    bin_src
-                } else {
-                    &pkg.name
-                };
-                if resolved_source == &package {
-                    let key = downloaded_list.release.req.codename.to_owned();
-                    if let Some(current_value) = versions.get_mut(&key) {
-                        if deb_version::compare_versions(current_value, &pkg.version)
-                            == Ordering::Greater
-                        {
-                            *current_value = pkg.version.to_owned();
-                        }
-                    } else {
-                        versions.insert(key, pkg.version.to_owned());
-                    }
-                }
-            }
-        }
-    }
-    println!("{:?}", versions);
-
-    let mut output_builder = Builder::default();
-    let mut sorted_by_version: Vec<_> = versions.iter().collect();
-
-    sorted_by_version.sort_by(|(_, v1), (_, v2)| deb_version::compare_versions(v1, v2));
-
-    for (codename, codename_version) in sorted_by_version {
-        output_builder.add_record(vec![
-            package.to_owned(),
-            codename_version.to_owned(),
-            codename.to_owned(),
-            "source".to_string(),
-        ]);
-    }
-    Ok(format!(
-        "{}\n",
-        output_builder
-            .build()
-            .with(Style::empty().vertical('|'))
-            .to_string()
-            .lines()
-            .map(|line| line.trim())
-            .collect::<Vec<&str>>()
-            .join("\n")
-    ))
-}
-
-fn init_system(config: Config) -> Result<System, anyhow::Error> {
-    // Setup the system
-    let mut system = System::cache_only()?;
-    commands::add_builtin_keys(&mut system);
-    system.add_sources_entries(sources_list::read(BufReader::new(
-        File::open(&config.sources_list).unwrap(),
-    ))?);
-
-    system.set_arches(vec!["amd64"]);
-    system.update()?;
-    Ok(system)
+    Ok(do_madison(package, system)?)
 }
 
 #[launch]
@@ -105,7 +33,7 @@ fn rocket() -> _ {
     let figment = rocket.figment();
     let config: Config = figment.extract().expect("config");
 
-    let system = init_system(config).expect("fapt System init");
+    let system = init_system(config.sources_list).expect("fapt System init");
 
     rocket
         .mount("/", routes![madison])
