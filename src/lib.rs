9a1ea3106ba7d09e70e686fc944ef5978601d01e
Daniel Watkins <daniel@daniel-watkins.co.uk>
2023-10-05 15:44:43 -0400

    madison-web: make parts of the web UI overridable

--- src/lib.rs
+++ src/lib.rs
@@ -374,45 +374,70 @@ pub mod madison_web {
                 madison_mapping: mapping_lock,
             })
             .attach(Template::try_custom(|engines| {
+                let loaded_templates: Vec<_> = engines
+                    .tera
+                    .get_template_names()
+                    .map(|s| s.to_string())
+                    .collect();
                 for (name, content) in templates::TEMPLATES {
-                    engines.tera.add_raw_template(name, content)?;
+                    if !loaded_templates.contains(&name.to_string()) {
+                        engines.tera.add_raw_template(name, content)?;
+                    }
                 }
                 Ok(())
             }))
     }
 
     mod templates {
+        const PACKAGE_MACROS: &str = r#"
+          {% macro package_row(record) %}
+          <tr>
+            <td>{{ record.package }}</td>
+            <td>{{ record.version }}</td>
+            <td>{{ record.codename }}</td>
+            <td>{{ record.architectures }}</td>
+          </tr>
+          {% endmacro package_row %}
+        "#;
+        const PACKAGE_TABLE: &str = r#"
+          {% import "package-macros" as package_macros %}
+          <table>
+            <thead>
+              <th>Package</th>
+              <th>Version</th>
+              <th></th>
+              <th>Architecture</th>
+            </thead>
+          {% for _, package_records in madison %}
+            {% for record in package_records %}
+              {{ package_macros::package_row(record=record) }}
+            {% endfor %}
+          {% endfor %}
+          </table>
+        "#;
+        const SEARCH_FORM: &str = r#"
+          <form method="get">
+            <input id="urlInput" type="search" name="package" placeholder="package name" autofocus required>
+            <input type="submit">
+          </form>
+        "#;
+
         const INDEX_TMPL: &str = r#"
             <html>
-              <form method="get">
-                <input id="urlInput" type="search" name="package" placeholder="package name" autofocus required>
-                <input type="submit">
-              </form>
+              {% include "search-form" %}
             </html>
         "#;
         const PACKAGE_TMPL: &str = r#"
             <html>
-              <table>
-                <thead>
-                  <th>Package</th>
-                  <th>Version</th>
-                  <th></th>
-                  <th>Architecture</th>
-                </thead>
-              {% for _, package_records in madison %}
-                {% for record in package_records %}
-                  <tr>
-                    <td>{{ record.package }}</td>
-                    <td>{{ record.version }}</td>
-                    <td>{{ record.codename }}</td>
-                    <td>{{ record.architectures }}</td>
-                  </tr>
-                {% endfor %}
-              {% endfor %}
-              </table>
+              {% include "package-table" %}
             </html>
         "#;
-        pub(super) const TEMPLATES: &[(&str, &str)] =
-            &[("index.html", INDEX_TMPL), ("package.html", PACKAGE_TMPL)];
+        pub(super) const TEMPLATES: &[(&str, &str)] = &[
+            ("package-macros", PACKAGE_MACROS),
+            ("package-table", PACKAGE_TABLE),
+            ("search-form", SEARCH_FORM),
+            ("index.html", INDEX_TMPL),
+            ("package.html", PACKAGE_TMPL),
+        ];
     }
 }
